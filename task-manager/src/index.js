4a7b6c9be11c130fa633ada38e0ba478b5a4d29b
Dushyant Sharma <dushyant.sharma@sarvika.com>
2021-11-12 20:07:57 +0530

    cors integration

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./router/user')
 const taskRouter = require('./router/task')
+var cors = require('cors')
 
 const app = express() 
 const PORT = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
